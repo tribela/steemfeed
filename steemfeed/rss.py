e95556a02d74336f9d5c205f8ee01379b6ceb7cf
kjwon15 <kjwonmail@gmail.com>
2018-02-12 10:20:13 +0900

    Change steemd node on post_compile stage

--- steemfeed/rss.py
+++ steemfeed/rss.py
@@ -9,15 +9,12 @@ from cache_expire import cache_with_timeout
 from mdx_gfm import GithubFlavoredMarkdownExtension
 
 md = markdown.Markdown(extensions=[GithubFlavoredMarkdownExtension()])
-steemd_nodes = [
-    'https://gtg.steem.house:8090',
-]
 
 
 @cache_with_timeout(60)
 def make_feed(userid):
 
-    s = steem.Steem(nodes=steemd_nodes)
+    s = steem.Steem()
     account = s.get_account(userid)
     profile = json.loads(account['json_metadata'])['profile']
 
