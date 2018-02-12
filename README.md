486759d2f8ed517b643dc25728ae76f5f4179294
kjwon15 <kjwonmail@gmail.com>
2018-02-12 10:19:50 +0900

    Edit metadata

---
+++ README.md
@@ -0,0 +1,5 @@
+# Steem feed
+
+Atom feed generator for Steemit blog
+
+Check https://steemfeed.kjwon15.net/@kjwon15
--- steemfeed/rss.py
+++ steemfeed/rss.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 import markdown
 import pyatom
@@ -16,16 +17,21 @@ steemd_nodes = [
 @cache_with_timeout(60)
 def make_feed(userid):
 
-    feed = pyatom.AtomFeed(
-        title=f'Steemit feed from {userid}',
-        url=f'https://steemit.com/@{userid}',
-        author=userid)
-
     s = steem.Steem(nodes=steemd_nodes)
+    account = s.get_account(userid)
+    profile = json.loads(account['json_metadata'])['profile']
+
+    feed = pyatom.AtomFeed(
+        author=profile.get('name', userid),
+        title=profile.get('name', userid),
+        subtitle=profile.get('about'),
+        url=f'https://steemit.com/@{userid}',
+        logo=profile['profile_image'])
 
     for post in s.get_discussions_by_blog({'tag': userid, 'limit': 10}):
         feed.add(
             title=post['title'],
+            author=post['author'],
             id=f'@kjwon15/{post["permlink"]}',
             url=f'https://steemit.com/{post["category"]}/@{post["author"]}/{post["permlink"]}',
             updated=datetime.strptime(post['last_update'], '%Y-%m-%dT%H:%M:%S'),
