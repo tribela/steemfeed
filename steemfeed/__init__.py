fed0d37b0bcb87feb842bbea1f1f59c3134429d9
kjwon15 <kjwonmail@gmail.com>
2018-02-16 09:11:15 +0900

    Attach sentry to log error

--- steemfeed/__init__.py
+++ steemfeed/__init__.py
@@ -1,9 +1,15 @@
+import os
+
 from flask import Flask, Response, request
+from raven.contrib.flask import Sentry
 
 from .rss import make_feed
 
+SENTRY_DSN = os.getenv('SENTRY_DSN')
 
 app = Flask(__name__)
+if SENTRY_DSN:
+    sentry = Sentry(app, dsn=SENTRY_DSN)
 
 
 @app.route('/@<userid>')
